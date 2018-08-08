113612b28d7e5ffb458038137fab71aa8af6ed2a
mskcd <mskcd.ru@gmail.com>
2018-08-08 10:48:30 +0300

    first commit

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#rm -r build;
+lektor build --output-path ./build;
+cp -r build/* /home/iv/doc/mskcd.github.io/;
+(cd /home/iv/doc/mskcd.github.io/ && git add .);
+(cd /home/iv/doc/mskcd.github.io/ && git commit -m"$1" );
+(cd /home/iv/doc/mskcd.github.io/ && git push && echo mskcd);
